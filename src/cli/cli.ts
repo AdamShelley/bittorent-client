fdd0267acfed76c792ce7fb5787c027f23797bbe
Adam Shelley <adam.shelley18@gmail.com>
2025-10-26 15:42:11 +0800

    Get headers from a .torrent file, create correct headers and retrieve a peer list.

--- src/cli/cli.ts
+++ src/cli/cli.ts
@@ -1,5 +1,7 @@
 import fs from "fs";
 import { decode } from "../bencode/bencode";
+import { headerAssesmbly } from "../header-assembly/headers";
+import { getPeerList } from "../http-requests/contact-tracker";
 
 const torrentPath = process.argv[2];
 
@@ -24,11 +26,34 @@ try {
   );
   process.exit(1);
 }
-const decoded = decode(buffer, 0);
 
-const announce = decoded?.decodedValue?.announce;
-if (announce && Buffer.isBuffer(announce)) {
-  console.log(announce.toString("utf8"));
-} else {
-  console.error("Error: 'announce' field is missing or not a Buffer.");
-}
+// DECODE FILE
+const searchString = "4:info";
+const position = buffer.indexOf(searchString);
+const infoStart = position + searchString.length;
+
+const decoded = decode(buffer, 0);
+const infoSection = decode(buffer, infoStart);
+
+if (!decoded) throw new Error("Decoding failed");
+if (!infoSection) throw new Error("No info section");
+const infoEnd = infoSection.index;
+const rawInfoBytes = buffer.subarray(infoStart, infoEnd);
+
+// ASSEMBLE HEADERS
+const headerAssesmblyResults = headerAssesmbly(
+  decoded.decodedValue,
+  rawInfoBytes
+);
+if (!headerAssesmblyResults) throw new Error("Assembling header failed");
+
+// GET PEER LIST, IDS, PORTS
+const peerList = getPeerList(headerAssesmblyResults);
+console.log(peerList);
+
+// TODO: Build the encoder and deocer for the peer protocol
+// TODO: Connect to all the peers, perform handshake, get bitfield - concurrently
+// TODO: Create a way to track all pieces
+// TODO: Download the pieces
+// TODO: Assemble the file, save, shutdown client
+// TODO: Bonus: Add support for seeding files, magnet URL, trackers, long running client to peer multiple files at a time
---
+++ src/header-assembly/headers.ts
@@ -0,0 +1,50 @@
+import crypto from "crypto";
+import { encode } from "../bencode/bencode";
+
+interface DecodedValueType {
+  announce: Buffer;
+  originalInfo: any;
+  info: {
+    length: number;
+  };
+}
+
+export interface HeaderReturnType {
+  url: string;
+  info_hash: Buffer;
+  peer_id: Buffer;
+  port: number;
+  uploaded: number;
+  downloaded: number;
+  left: number;
+  compact: number;
+}
+
+export const headerAssesmbly = (
+  decodedValue: DecodedValueType,
+  rawInfoBytes: Buffer
+): HeaderReturnType => {
+  const announceURL = decodedValue.announce;
+  const length = decodedValue.info.length;
+
+  const hashedInfo = crypto.createHash("sha1").update(rawInfoBytes).digest();
+
+  console.log(hashedInfo);
+
+  return {
+    url: announceURL.toString("utf8"),
+    info_hash: hashedInfo,
+    peer_id: createPeerId(),
+    port: 6881,
+    uploaded: 0,
+    downloaded: 0,
+    left: length,
+    compact: 1,
+  };
+};
+
+const createPeerId = () => {
+  const prefix = Buffer.from("-AS0001-");
+  const random = crypto.randomBytes(12);
+  return Buffer.concat([prefix, random]);
+};
